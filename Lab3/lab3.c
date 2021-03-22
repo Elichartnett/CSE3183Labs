c263bd49e388a536179c2b26f9f44207902a7ed0
Eli Hartnett <55256366+Elichartnett@users.noreply.github.com>
2021-03-21 21:58:58 -0500

    Update lab3.c
    with pass by reference

--- Lab3/lab3.c
+++ Lab3/lab3.c
@@ -6,12 +6,19 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 
-int run_command_in_subprocess(char *file, char *argv_new[4]);
-void printout_terminated_subprocess(int pfds, char *file_name);
+#define F_SETFL 4
+#define F_GETFL 3
+#define O_NONBLOCK 0x00000004
+
+void run_command_in_subprocess(char *argv_new[4], int pipe[], int *pid);
+void printout_terminated_subprocess(char *file_name, int rpipe);
 
 int main(int argc, char *argv[])
 {
+    int errors = 0;
+
     if ((argc - 1) < 4)
     {
         printf("%s %d %s", "Error: Only", argc - 1, "arguments given. To use runpar, use the following syntax: ./runpar NUMCORES COMMAND... _files_ FILE...\n");
@@ -23,6 +30,11 @@ int main(int argc, char *argv[])
         char *command = argv[2];
 
         int first_file = 4;
+
+        char *argv_new[4];
+        argv_new[0] = command;
+        argv_new[3] = NULL;
+
         char *option = NULL;
         if (strcmp(argv[3], "_files_") != 0) //command has -option
         {
@@ -30,83 +42,116 @@ int main(int argc, char *argv[])
             first_file++;
         }
 
-        int running = 0;
-        int num_files = argc - first_file;
-        int pfds[num_files];
         int file_num = 0;
+        int num_files = argc - first_file;
+        int running = 0;
+        int pipes[num_files][2]; //index 0: read pipe fd, index 1: write pipe fd
         int num_printed = 0;
+
+        int pids[num_files];
+
         while (file_num < num_files)
         {
             if (running < num_cores)
             {
-                char *argv_new[4];
-                argv_new[0] = command;
-                argv_new[1] = option;
-                argv_new[2] = argv[first_file++];
-                argv_new[3] = NULL;
+                char *file = argv[first_file];
+
+                if (option != NULL)
+                {
+                    argv_new[1] = option;
+                    argv_new[2] = file;
+                }
+                else
+                {
+                    argv_new[1] = file;
+                    argv_new[2] = NULL;
+                }
+
+                pipe(pipes[file_num]); // need to error check
+                fcntl(pipes[file_num][0], F_SETFL, fcntl(pipes[file_num][0], F_GETFL) | O_NONBLOCK);
+                fcntl(pipes[file_num][1], F_SETFL, fcntl(pipes[file_num][0], F_GETFL) | O_NONBLOCK);
+
+                run_command_in_subprocess(argv_new, pipes[file_num], &pids[file_num]);
 
                 running++;
-                pfds[file_num++] = run_command_in_subprocess(argv_new[2], argv_new);
+                first_file++;
+                file_num++;
             }
             else
             {
-                wait(NULL);
-                running--;
-                printout_terminated_subprocess(pfds[num_printed], argv[argc - num_files + num_printed]);
-                num_printed++;
+                int status = 0;
+                int pid = wait(&status);
+                printf("PID finished: %d\n", pid);
+                if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
+                {
+                    printout_terminated_subprocess(argv[argc - num_files + num_printed], pipes[num_printed][0]); //FIX - need data structure mapping PID to pipe
+
+                    num_printed++;
+                    running--;
+                }
+                else
+                    errors = 1;
             }
         }
-
         if (running > 0)
         {
             for (int i = 0; i < running; i++)
             {
-                wait(NULL);
-                printout_terminated_subprocess(pfds[num_printed], argv[argc - num_files + num_printed]);
-                num_printed++;
+                int status = 0;
+                int pid = wait(&status);
+                if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
+                {
+                    printout_terminated_subprocess(argv[argc - num_files + num_printed], pipes[num_printed][0]); //FIX - need data structure mapping PID to pipe - possible idea: pass by reference to get pid
+                    num_printed++;
+                }
+                else
+                    errors = 1;
             }
         }
-    }
-    return EXIT_SUCCESS;
-}
-
-int run_command_in_subprocess(char *file, char *argv_new[4])
-{
-    int pfds[2];
-    if (pipe(pfds) == -1)
-    {
-        //error creating pipe
-    }
-    else
-    {
-        switch (fork())
+        for (int i = 0; i < num_files; i++)
         {
-        case -1:
-            perror("Fork failed");
-            exit(EXIT_FAILURE);
-
-        case 0: //child process
-            //close(pipe(&pfds[0]));
-            dup2(pfds[1], 1); //fd 1 is standard output - redirects to pipe write end (pfds[1])
-            execvp(argv_new[0], argv_new);
-            exit(EXIT_FAILURE);
-
-        default: //parent process
-            //close(pipe(&pfds[1]));
-            return pfds[0];
-            break;
+            printf("%d\n", pids[i]);
         }
     }
-    return pfds[0];
+    if (errors == 0)
+        return EXIT_SUCCESS;
+    else
+        return EXIT_FAILURE;
 }
 
-void printout_terminated_subprocess(int pfds, char *file_name)
+void run_command_in_subprocess(char *argv_new[4], int pipe[], int *pid)
+{
+    switch (fork())
+    {
+    case -1:
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+
+    case 0: //child process
+        *pid = getpid();
+        close(pipe[0]);
+        dup2(pipe[1], 1); //fd 1 is standard output - redirects to pipe write end (pfds[1])
+        execvp(argv_new[0], argv_new);
+        exit(EXIT_FAILURE);
+
+    default: //parent process
+        close(pipe[1]);
+        break;
+    }
+}
+
+void printout_terminated_subprocess(char *file_name, int rpipe)
 {
     printf("--------------------\n");
     printf("%s%s\n", "Output from: ", file_name);
     printf("--------------------\n");
 
     char buff[4096];
-    int nread = read(pfds, buff, 4096);
-    write(1, buff, nread);
+    int nread;
+    while ((nread = read(rpipe, buff, 4096)) > 0)
+        write(1, buff, nread);
+    if (nread == -1)
+    {
+        //FIX - see if there was an error while reading or just nothing to read
+    }
 }

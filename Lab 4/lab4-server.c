c95c41cea79f303aa45413311b581d8037ca2bc4
Eli Hartnett <55256366+Elichartnett@users.noreply.github.com>
2021-04-10 14:32:22 -0500

    Update lab4-server.c

--- Lab 4/lab4-server.c
+++ Lab 4/lab4-server.c
@@ -63,10 +63,9 @@ int main(int argc, char *argv[])
         else
         {
             dup2(connect_fd, 1);
-
+            dup2(connect_fd, 2);
             handle_client(connect_fd);
-            close(connect_fd);
-            printf("--------------------\n");
+
         }
     }
     return EXIT_SUCCESS;
@@ -78,6 +77,7 @@ void handle_client(int connect_fd)
     {
         perror("Writing <remps> failed\n");
         close(connect_fd);
+        return;
     }
 
     int nread;
@@ -85,29 +85,29 @@ void handle_client(int connect_fd)
     if ((nread = read(connect_fd, received_secret, strlen(secret))) < 0) //Protocol: Verify <shared secret>
     {
         perror("Reading <shared secret> failed\n");
-        close(connect_fd);
+        return;
     }
     else
     {
         received_secret[nread] = '\0';
-        if (strcmp(received_secret, secret) != 0)
+        if (strcmp(received_secret, secret) != 0) //Incorrect shared secret
         {
-            printf("Incorrect shared secret\n");
-            close(connect_fd);
+            printf("Invalid shared secret\n");
+            return;
         }
     }
 
     if (write(connect_fd, "<ready>", 7) < 0) //Protocol: Send <ready>
     {
         perror("Write <ready> failed\n");
-        close(connect_fd);
+        return;
     }
 
     char *directive = malloc(50);                      //NEED TO ADD DYNAMIC MEMORY
     if ((nread = read(connect_fd, directive, 56)) < 0) //Protocol: Get directive (user, cpu, or mem)
     {
         perror("Reading directive (user, cpu, or mem) failed\n");
-        close(connect_fd);
+        return;
     }
     else
     {
@@ -117,10 +117,10 @@ void handle_client(int connect_fd)
         {
             char *user_name = directive + 6;
             struct passwd *user_check = getpwnam(user_name);
-            if (user_check == NULL)
+            if (user_check == NULL) //Invalid username
             {
-                printf("Invalid username\n");
-                close(connect_fd);
+                printf("Invalid user name\n");
+                return;
             }
             else
             {
@@ -134,24 +134,15 @@ void handle_client(int connect_fd)
         }
         else if ((strcmp(directive, "<cpu>")) == 0) //Directive is cpu
         {
-            printf("8. Received <cpu>:\n");
-
-            printf("9. Sending output to client\n");
-            dup2(connect_fd, 1);
-            dup2(connect_fd, 2);
             char *command = "ps -NT -o pid,ppid,%cpu,%mem,args --sort -%cpu | head";
             system(command);
         }
         else if ((strcmp(directive, "<mem>")) == 0) //Directive is mem
         {
-            printf("8. Received <mem>:\n");
-
-            printf("9. Sending output to client\n");
-            dup2(connect_fd, 1);
-            dup2(connect_fd, 2);
             char *command = "ps -NT -o pid,ppid,%cpu,%mem,args --sort -%mem | head";
             system(command);
         }
     }
     free(directive);
+    close(connect_fd);
 }

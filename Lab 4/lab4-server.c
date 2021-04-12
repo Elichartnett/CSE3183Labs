f277aac39477c7447fd75b8dac96dcc252842ebc
Eli Hartnett <55256366+Elichartnett@users.noreply.github.com>
2021-04-12 15:50:20 -0500

    Update lab4-server.c

--- Lab 4/lab4-server.c
+++ Lab 4/lab4-server.c
@@ -16,59 +16,67 @@ void handle_client(int connect_fd);
 
 int main(int argc, char *argv[])
 {
-    int listen_fd, connect_fd;
-    struct sockaddr_in servaddr;
+    if (argc == 1)
+    {
+        int listen_fd, connect_fd;
+        struct sockaddr_in servaddr;
 
-    if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) //Creating socket
-    {
-        perror("socket() failed\n");
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        int i = 1;
-        if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i)) < 0) //Setting socket option to reuse address
+        if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) //Creating socket
         {
-            perror("setsockopt() failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    servaddr.sin_family = AF_INET; //Setting up server port and address
-    servaddr.sin_port = htons(port);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) //Binding socket to port and address
-    {
-        perror("bind() failed");
-        close(listen_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    if (listen(listen_fd, 10) != 0) //Listening for connections
-    {
-        perror("listen() failed");
-        close(listen_fd);
-        exit(EXIT_FAILURE);
-    }
-
-    while (1)
-    {
-        connect_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL); //Client connected
-        if (connect_fd < 0)
-        {
-            perror("accept() failed");
+            perror("socket() failed\n");
             exit(EXIT_FAILURE);
         }
         else
         {
-            dup2(connect_fd, 1);
-            dup2(connect_fd, 2);
-            handle_client(connect_fd);
-            close(connect_fd);
+            int i = 1;
+            if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &i, sizeof(i)) < 0) //Setting socket option to reuse address
+            {
+                perror("setsockopt() failed");
+                exit(EXIT_FAILURE);
+            }
         }
+
+        servaddr.sin_family = AF_INET; //Setting up server port and address
+        servaddr.sin_port = htons(port);
+        servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+        if (bind(listen_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) //Binding socket to port and address
+        {
+            perror("bind() failed");
+            close(listen_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        if (listen(listen_fd, 10) != 0) //Listening for connections
+        {
+            perror("listen() failed");
+            close(listen_fd);
+            exit(EXIT_FAILURE);
+        }
+
+        while (1)
+        {
+            connect_fd = accept(listen_fd, (struct sockaddr *)NULL, NULL); //Client connected
+            if (connect_fd < 0)
+            {
+                perror("accept() failed");
+                exit(EXIT_FAILURE);
+            }
+            else
+            {
+                dup2(connect_fd, 1);
+                dup2(connect_fd, 2);
+                handle_client(connect_fd);
+                close(connect_fd);
+            }
+        }
+        return EXIT_SUCCESS;
     }
-    return EXIT_SUCCESS;
+    else
+    {
+        printf("Usage: ./rempsd\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void handle_client(int connect_fd)
@@ -90,8 +98,11 @@ void handle_client(int connect_fd)
     else
     {
         received_secret[nread] = '\0';
-        if (strcmp(received_secret, secret) != 0) //Incorrect shared secret
+        if (strcmp(received_secret, secret) != 0)
+        {
+            printf("Incorrect secret\n");
             return;
+        }
     }
 
     if (write(connect_fd, "<ready>", 7) < 0) //Protocol: Send <ready>
@@ -100,8 +111,8 @@ void handle_client(int connect_fd)
         return;
     }
 
-    char *directive = malloc(42); //Linux useradd man page sets max username length to 35; max directive is length 6 (<user>); 35 + 6 + null char = 42
-    if ((nread = read(connect_fd, directive, 56)) < 0) //Protocol: Get directive (user, cpu, or mem)
+    char *directive = malloc(43);                      //Linux useradd man page sets max username length to 35; max directive is length 6 (<user:>); 35 + 7 + null char = 43
+    if ((nread = read(connect_fd, directive, 42)) < 0) //Protocol: Get directive (user, cpu, or mem)
     {
         perror("Reading directive (user, cpu, or mem) failed\n");
         return;
@@ -110,9 +121,10 @@ void handle_client(int connect_fd)
     {
         directive[nread] = '\0';
 
-        if ((strncmp(directive, "<user>", 6)) == 0) //Directive is user
+        if ((strncmp(directive, "<user:", 6)) == 0) //Directive is user
         {
             char *user_name = directive + 6;
+            user_name[strlen(user_name)-1] = '\0';
             struct passwd *user_check = getpwnam(user_name);
             if (user_check == NULL) //Invalid username
                 return;
